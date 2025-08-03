e4c44e2bf83d1193917886f72b77b6fb43c44b62
Divesh31 <diveshpal31@gmail.com>
2025-08-03 17:19:10 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,21 @@
+# Task 1
+num1=input('Enter the First number :')
+num2=input('Enter the Second number :')
+num1=float(num1)
+num2=float(num2)
+add = num1+num2
+sub =num1-num2
+mul =num1*num2
+div =num1/num2
+print("addition :" ,add)
+print("subtraction",sub)
+print("mulyiplication",mul)
+print("divide",div)
+
+
+# Task 2
+first =input('Enter your First name :')
+last =input('Enter your last name :')
+full =first +last
+print('Full Name :',full)
+print("Hello,",full + "! welcom to the python program")
